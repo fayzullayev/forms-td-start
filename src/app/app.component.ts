f53f931bedab27bf94385e529add1c945f0bc7a6
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-03-16 14:49:57 +0500

    tdd has ended

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('form') form: NgForm;
+  defaultQuestion = 'pet';
+  public answer: string = '';
+
+  genders = ['male', 'female'];
+
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    questionAnswer: '',
+    gender: '',
+  };
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // this.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'odilfayz22gmail.com',
+    //   },
+    //   secret: 'teacher',
+    //   questionAnswer: 'qwertyuiop',
+    //   gender: 'female',
+    // });
+
+    this.form.form.patchValue({
+      userData: {
+        email: 'exdcfvtghbunjimk,l.;',
+      },
+    });
+  }
+
+  onSubmit() {
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secret = this.form.value.secret;
+    this.user.questionAnswer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+
+    this.form.reset();
   }
 }
